477c04af89e72af017d3f36a264c462e73c9b66c
Stephan Heilner <stephanheilner@gmail.com>
2024-06-10 12:23:13 -0600

    Fix emptystateview

--- Sources/Views/EmptyStateView.swift
+++ Sources/Views/EmptyStateView.swift
@@ -26,8 +26,12 @@ import Foundation
 import SwiftUI
 
 public struct EmptyStateView<Content: View>: View {
-    @ViewBuilder public let content: Content
+    @ViewBuilder let content: () -> Content
 
+    public init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
     public var body: some View {
         HStack {
             Spacer()
@@ -35,7 +39,7 @@ public struct EmptyStateView<Content: View>: View {
             VStack {
                 Spacer()
 
-                content
+                content()
                     .padding(50)
 
                 Spacer()
